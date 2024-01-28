140e6d7e181f4cf928288a30f356ba1607b70dee
suhwan-cheon <soo7652@naver.com>
2024-01-28 22:31:43 +0900

    ADD: 초기 화면 구조 설계

---
+++ src/apis/client.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+
+export const tmp = () => {
+  console.log('tmp')
+};
