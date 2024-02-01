5bd82e74e25465afff0d31e7e0ad593863e99c9a
minseok.kim <kmus1232@gmail.com>
2024-02-01 21:49:09 +0900

    logo changed for keyreer logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,30 @@
-import React from "react";
-import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { View, Image, useTheme } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import logo from "../assets/logo.svg";
 
-import awsExports from "../aws-exports.js";
-Amplify.configure(awsExports);
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image alt="keyreer logo" src={logo} />
+      </View>
+    );
+  },
+};
 
 export default function Login() {
   return (
     <Authenticator
       loginMechanisms={["email"]}
       signUpAttributes={["nickname"]}
-      socialProviders={["google"]}
+      components={components}
     >
       {({ signOut, user }) => (
         <main>
-          <h1>Hello</h1>
+          <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
