7c52ede94bf44e588a6d90e9e2d83520e6ca4850
minseok.kim <kmus1232@gmail.com>
2024-02-01 19:07:16 +0900

    init amplify

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,24 @@
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "../aws-exports.js";
+Amplify.configure(awsExports);
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <Authenticator
+      loginMechanisms={["email"]}
+      signUpAttributes={["nickname"]}
+      socialProviders={["google"]}
+    >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
