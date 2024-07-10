6aa8144abc91aeae1703b1d2f210f05a245696e9
minseok kim <kimminseok@kimminseok.local>
2024-07-10 11:49:41 +0900

    Fix: Datadog RUM setup

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,6 @@ import Keyword from "./pages/Keyword";
 import Login from "./pages/Login";
 
 import "./App.css";
-
-import { datadogRum } from '@datadog/browser-rum';
-
-datadogRum.init({
-    applicationId: '06f159b6-82df-41d8-bfd3-71b44fe6ad42',
-    clientToken: 'pub19f71f64d5682a70ba36a9ec703a3e74',
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
-    site: 'datadoghq.com',
-    service: 'keyreer-frontend',
-    env: 'prod',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0', 
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 100,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: 'mask-user-input',
-});
-
 Amplify.configure(config);
 
 const App = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,28 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '06f159b6-82df-41d8-bfd3-71b44fe6ad42',
+    clientToken: 'pub19f71f64d5682a70ba36a9ec703a3e74',
+    // `site` refers to the Datadog site parameter of your organization
+    // see https://docs.datadoghq.com/getting_started/site/
+    site: 'datadoghq.com',
+    service: 'keyreer-frontend',
+    env: 'prod',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    // version: '1.0.0', 
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
+
+
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
